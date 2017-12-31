83c48ea0a2a630165e76f9180cf3b598b6f4f1af
Manuel Bogner <manuel.bogner@openresearch.com>
2017-12-31 12:32:39 +0100

    add missing spring boot plugin and fix test

--- lib/src/test/java/pm/mbo/jwt/auth/CacheTest.java
+++ lib/src/test/java/pm/mbo/jwt/auth/CacheTest.java
@@ -2,8 +2,10 @@ package pm.mbo.jwt.auth;
 
 import org.ehcache.Cache;
 import org.junit.Test;
+import org.springframework.util.ReflectionUtils;
 import pm.mbo.jwt.JwtConfig;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,7 +15,11 @@ public class CacheTest {
 	private final JwtConfig cacheConfig = new JwtConfig();
 
 	@Test
-	public void testCache() {
+	public void testCache() throws Exception {
+		final Field field = ReflectionUtils.findField(JwtConfig.class, "noncePoolSize");
+		ReflectionUtils.makeAccessible(field);
+		ReflectionUtils.setField(field, cacheConfig, 10000L);
+
 		final Cache<String, LocalDateTime> nonceCache = cacheConfig.nonceCache(cacheConfig.cacheManager());
 		final String nonce1 = "a";
 
